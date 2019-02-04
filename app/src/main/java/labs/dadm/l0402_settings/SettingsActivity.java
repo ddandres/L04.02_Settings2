0292fd6d3c2a3ee9b186ac8ac1ad1b3963c01bce
ddandres <ddandres@disca.upv.es>
2019-02-04 11:20:13 +0100

    Update to DADM, API 28

--- app/src/main/java/labs/dadm/l0402_settings/SettingsActivity.java
+++ app/src/main/java/labs/dadm/l0402_settings/SettingsActivity.java
@@ -19,42 +19,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         // This method is deprecated in favour of the modern Fragment-based activities,
         // but it is perfectly safe to use it
         addPreferencesFromResource(R.xml.preferences);
-        // Include the ActionBar for up navigation
-        setupActionBar();
-    }
-
-    /*
-        This method displays the up navigation arrow if an ActionBar is available
-     */
-    private void setupActionBar() {
-        // Get a reference to the ActionBar
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    /*
-        This method is executed when any action from the ActionBar is selected
-    */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Determine the action to take place according to the Id of the action selected
-        int id = item.getItemId();
-        /*
-            Up navigation
-            NOTE: This is not required usually, but due to the particular implementation of
-            AppCompatPreferenceActivity class, it is necessary to explicitly state the action
-            to take when the up navigation option is selected
-        */
-        if (id == android.R.id.home) {
-            // Navigate from this activity to its parent (they must be located in the same task)
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
